3e22efef5aa8ad9670f0d434adfdf6212863efb9
JeromeGlt <jerome.gallot39@orange.fr>
2023-02-23 18:15:14 +0100

    creation of a PUT route, to modify a movie

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -4,7 +4,7 @@ exports.getAllMovies = (req, res, next) => {
 
   Movie.findAll()
   .then(movies => res.status(200).json(movies))
-  .catch(error => res.status(400).json(error))
+  .catch(err => res.status(400).json(err))
 }
 
 exports.createMovie = (req, res, next) => {
@@ -17,16 +17,32 @@ exports.createMovie = (req, res, next) => {
     decade: req.body.decade
   })
   .then(movie => res.status(200).json({ movie }))
-  .catch(error => res.status(404).json(error))
+  .catch(err => res.status(404).json(err))
   console.log(movie)
 }
 
+exports.modifyMovie = (req, res, next) => {
+
+  const movieObject = {
+    title: req.body.title,
+    director: req.body.director,
+    actor: req.body.actor,
+    year: req.body.year,
+    decade: req.body.decade
+  }
+
+  Movie.update({ ...movieObject }, { where : { id : req.params. id }})
+  .then(() => res.status(200).json({ message : 'Amended movie' }))
+  .catch(err => res.status(404).json(err))
+}
+
 exports.deleteMovie = (req, res, next) => {
+
   Movie.findOne({ where : { id : req.params.id }})
   .then(movie =>
     Movie.destroy({ where : { id : req.params.id }})
     .then(() => res.status(200).json({ message : 'Deleted movie' }))
-    .catch(error => res.status(404).json(error))
+    .catch(err => res.status(404).json(err))
   )
-  .catch(error => res.status(404).json(error))
+  .catch(err => res.status(404).json(err))
 }
--- routes/movieRoute.js
+++ routes/movieRoute.js
@@ -7,7 +7,7 @@ const movieController = require('../controllers/movieController')
 router.get('/', movieController.getAllMovies)
 router.post('/', movieController.createMovie)
 router.delete('/:id', movieController.deleteMovie)
+router.put('/:id', movieController.modifyMovie)
 // router.get('/post/:id', auth, PostCtrl.getOnePost)
-// router.put('/:id', auth, multer, PostCtrl.modifyPost)
 
 module.exports = router
