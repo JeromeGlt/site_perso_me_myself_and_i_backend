ed5a33b73293d997a6557b7678cc44ed7a370e6c
JeromeGlt <jerome.gallot39@orange.fr>
2023-03-01 10:42:15 +0100

    creation of a POST route, to create a log in for the users

--- controllers/userController.js
+++ controllers/userController.js
@@ -45,3 +45,58 @@ exports.signup = (req, res, next) => {
   })
   .catch((err) => res.status(500).json(err))  
 }
+
+exports.login = (req, res, next) => {
+  User.findOne({ where : { username: req.body.username }})
+  .then(user => {
+    if(!user) {
+      return res.status(401).json({ message : 'Utilisateur non trouvé' })
+    }
+    bcrypt.compare(req.body.password, user.password)
+    .then(valid => {
+      if(!valid) {
+        return res.status(401).json({ message : 'Mot de passe incorrect' })
+      }
+      res.status(200).json({
+        userId: user.id,
+        username: user.username,
+        token: jwt.sign(
+          {userId: user.id},
+          process.env.TOKEN,
+          {expiresIn: '24h'}
+        )
+      })
+    })
+    .catch((err) => res.status(500).json(err))
+  })
+  .catch((err) => res.status(500).json(err))
+}
+
+// exports.modifyProfile = (req, res, next) => {
+//   const userObject = req.file ? {
+//     ...req.body,
+//     imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+//   } : {
+//     ...req.body
+//   }
+
+//   User.update({ ...userObject }, { where : { id : req.params.id }})
+//   .then(() => res.status(200).json({ message : 'Utilisateur modifié' }))
+//   .catch((error) => res.status(500).json({ error }))
+// }
+
+// exports.delete = (req, res, next) => {
+//   User.findOne({ where : { pseudo : req.params.pseudo }})
+//   .then(user => {
+//       if(!user) {
+//           return res.status(401).json({ message : 'Utilisateur non trouvé' })
+//       }
+//       const filename = user.imageUrl.split('/images/')[1]
+//       fs.unlink(`images/${filename}`, () => {
+//           User.destroy({ where : { pseudo : req.params.pseudo }})
+//           .then(() => res.status(200).json({ message : 'Utilisateur supprimé !' }))
+//           .catch(() => res.status(500).json({ message : 'Suppression impossible !' }))
+//       })
+//   })
+//   .catch((error) => res.status(500).json({ error }))
+// }
