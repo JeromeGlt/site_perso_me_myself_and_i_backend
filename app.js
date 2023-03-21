1dd26c28f465382f5aca42abf39e06afe67416f5
JeromeGlt <jerome.gallot39@orange.fr>
2023-03-21 17:45:26 +0100

    debug of the origin of the images

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(cors())
 app.use(helmet())
 
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
+app.use('/images', express.static(__dirname + '/images'))
 
 app.use(express.json())
 
